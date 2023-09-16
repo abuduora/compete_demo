3bdb9f62f6d6669d7b13fbd573f59a916a5cef76
Cc157486 <1574860363@qq.com>
2023-09-16 21:57:41 +0800

    主要是照相功能

--- Goodneighbor/app/src/main/java/com/example/goodneighbor/Activity/Mine/AboutActivity.java
+++ Goodneighbor/app/src/main/java/com/example/goodneighbor/Activity/Mine/AboutActivity.java
@@ -2,18 +2,31 @@ package com.example.goodneighbor.Activity.Mine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.goodneighbor.R;
 
 public class AboutActivity extends AppCompatActivity {
     private Button btn_About;
+    private View button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mine_about);
       /*  btn_About=findViewById(R.id.btn_About);*/
-
+//跳转到我的
+        button = findViewById(R.id.btn_back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent();
+                intent.setClass(AboutActivity.this,MineActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Goodneighbor/app/src/main/java/com/example/goodneighbor/Activity/Mine/EditMessageActivity.java
+++ Goodneighbor/app/src/main/java/com/example/goodneighbor/Activity/Mine/EditMessageActivity.java
@@ -1,16 +1,66 @@
 package com.example.goodneighbor.Activity.Mine;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.goodneighbor.R;
 
-public class EditMessageActivity extends AppCompatActivity {
+public class EditMessageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View button;
+    private View iv_image;
+    private ActivityResultLauncher<Intent> mRresultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mine_message);
+        iv_image = findViewById(R.id.iv_image);
+        iv_image.setOnClickListener(this);
+        //跳转到系统相册，选择图片，并返回
+        mRresultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+            }
+        });
+
+        //跳转到我的
+        button = findViewById(R.id.btn_back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent();
+                intent.setClass(EditMessageActivity.this,MineActivity.class);
+                startActivity(intent);
+            }
+        });
+        button = findViewById(R.id.btn_flish);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent();
+                intent.setClass(EditMessageActivity.this,MineActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+    //添加头像
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.iv_image){
+            Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            mRresultLauncher.launch(intent);
+    }
+}
 }
